10e17a99a11bbe2676eee418d33aa209266b767b
Akshay Karle <akshay.a.karle@gmail.com>
2017-08-15 16:40:59 -0300

    module that provisions ecs service with an alb listener

---
+++ service_with_alb_listener/main.tf
@@ -0,0 +1,64 @@
+variable "vpc_id" {}
+variable "cluster_id" {}
+variable "alb_arn" {}
+variable "alb_dns_name" {}
+variable "alb_zone_id" {}
+variable "iam_role_arn" {}
+variable "route53_name" {}
+variable "route53_zone_id" {}
+variable "name" {}
+variable "ports" { type = "list" }
+variable "health_check_path" { default = "/" }
+variable "task_definition" {}
+
+resource "aws_alb_target_group" "default" {
+  count = "${length(var.ports)}"
+  name       = "${var.name}-${terraform.workspace}-${element(var.ports, count.index)}"
+  tags { env = "${terraform.workspace}" }
+  port       = "${element(var.ports, count.index)}"
+  protocol   = "HTTP"
+  vpc_id     = "${var.vpc_id}"
+
+  health_check {
+    path = "${var.health_check_path}"
+  }
+}
+
+resource "aws_alb_listener" "default" {
+  count = "${length(var.ports)}"
+  load_balancer_arn = "${var.alb_arn}"
+  port       = "${element(var.ports, count.index)}"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = "${element(aws_alb_target_group.default.*.id, count.index)}"
+    type             = "forward"
+  }
+}
+
+resource "aws_route53_record" "default" {
+  zone_id = "${var.route53_zone_id}"
+  name    = "${var.name}.${var.route53_name}"
+  type    = "A"
+
+  alias {
+    name                   = "${var.alb_dns_name}"
+    zone_id                = "${var.alb_zone_id}"
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_ecs_service" "service" {
+  depends_on = ["aws_alb_listener.default"]
+  name            = "${var.name}"
+  cluster         = "${var.cluster_id}"
+  task_definition = "${var.task_definition}"
+  iam_role = "${var.iam_role_arn}"
+  desired_count   = 1
+
+  load_balancer {
+    target_group_arn = "${aws_alb_target_group.default.0.arn}"
+    container_name   = "${var.name}"
+    container_port = "${element(var.ports, 0)}"
+  }
+}
